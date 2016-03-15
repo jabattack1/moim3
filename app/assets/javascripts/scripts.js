c03ef9940b996e4c784c2f6a4a040ca99f5df7a3
jabattack1 <peter.hahn@live.com>
2016-03-16 01:07:13 +0900

    signup

--- app/assets/javascripts/scripts.js
+++ app/assets/javascripts/scripts.js
@@ -1,6 +1,11 @@
 console.log("connected")
 
-
+function isNumberKey(evt){
+    var charCode = (evt.which) ? evt.which : event.keyCode
+    if (charCode > 31 && (charCode < 48 || charCode > 57))
+        return false;
+    return true;
+}
 
 
 
