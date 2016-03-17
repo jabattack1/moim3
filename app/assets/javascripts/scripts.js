403bee9cef9b700f99b080825a24d3e30730333e
jabattack1 <peter.hahn@live.com>
2016-03-17 16:19:23 +0900

    mission page

--- app/assets/javascripts/scripts.js
+++ app/assets/javascripts/scripts.js
@@ -7,7 +7,10 @@ function isNumberKey(evt){
     return true;
 }
 
-
+function validEmail(v) {
+    var r = new RegExp("[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?");
+    return (v.match(r) == null) ? false : true;
+}
 
 
 
@@ -3596,4 +3599,5 @@ function closePrivacyWelcome(){
 	console.log('hit');	
 	$('#privacyPolicyWelcome').remove(); 
 }
+
 
