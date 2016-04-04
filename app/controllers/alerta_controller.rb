a4cd3a6fa114fd4bfae2843eb6062079516b3bee
jabattack1 <peter.hahn@live.com>
2016-04-04 19:40:02 +0900

    logo link

--- app/controllers/alerta_controller.rb
+++ app/controllers/alerta_controller.rb
@@ -11,17 +11,25 @@ def index
 end
 
 def create
+ user_item = User.find_by(email: params['email'])
 
-  user_item = User.find_by(email: params['email'])
-
-    if user_item
-      session[:username] = user_item
-      # redirect_to '/alerta'
-      redirect_to(controller: 'alerta')
-    else
-    	return
-    end
-
+    
+  if user_item
+    render :status => 400
+  else
+    render json: nil, status: :ok
+  end
+  #   if user_item
+  #     session[:username] = user_item
+  #     # redirect_to '/alerta'
+  #     redirect_to(controller: 'alerta')
+  #   else
+  #   	return
+  #   end
 end
+
+# def check_already_email
+#     @email = users.search(params[:email])
+# end
 
 end
--- app/controllers/alertb_controller.rb
+++ app/controllers/alertb_controller.rb
@@ -12,16 +12,24 @@ end
 
 def create
 
-    user_item1 = User.find_by(username: params['username'])
-    if user_item1
-  		session[:username] = user_item1
-      # @user=session[:username]
-      # redirect_to '/alertb'
-      redirect_to(controller: 'alertb')
-      return
-    else
-    	return
-    end
+  user_item = User.find_by(username: params['username'])
+    
+  if user_item
+    render :status => 400
+  else
+    render json: nil, status: :ok
+  end
+
+    # user_item1 = User.find_by(username: params['username'])
+    # if user_item1
+  		# session[:username] = user_item1
+    #   # @user=session[:username]
+    #   # redirect_to '/alertb'
+    #   redirect_to(controller: 'alertb')
+    #   return
+    # else
+    # 	return
+    # end
 
 end
 
