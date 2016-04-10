96b5be242d58dbb1873074a8de3da0329394e4b0
jabattack1 <peter.hahn@live.com>
2016-04-10 20:24:01 +0900

    heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,19 @@
 Rails.application.configure do
+
+    config.action_mailer.smtp_settings = {
+    :address              => "smtp.sendgrid.net",
+    :port                 => 587,
+    # :domain               => "sendgrid.com",   
+    :user_name            => ENV['USERNAME'],
+    :password             => ENV['PASSWORD'],
+    :authentication       => 'plain',
+    :enable_starttls_auto => true  }
+
+
+config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+config.action_mailer.delivery_method = :smtp
+
+config.action_mailer.perform_deliveries = true
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
