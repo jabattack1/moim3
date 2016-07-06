852e5a305b683306733caafe043e8ec82675e8b8
jabattack1 <peter.hahn@live.com>
2016-07-06 18:48:36 +0900

    table

--- db/schema.rb
+++ db/schema.rb
@@ -788,12 +788,5 @@ ActiveRecord::Schema.define(version: 20160705094940) do
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
-  create_table "videos", force: :cascade do |t|
-    t.string   "title"
-    t.string   "description"
-    t.string   "link"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
-  end
 
 end
